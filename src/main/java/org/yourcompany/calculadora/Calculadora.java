dd95add0f9ff4b4958c1672777029e77b4dd149d
hoshikuu <145494860+Hoshikuu@users.noreply.github.com>
2025-11-14 10:33:04 +0100

    Versio definitiva

--- src/main/java/org/yourcompany/calculadora/Calculadora.java
+++ src/main/java/org/yourcompany/calculadora/Calculadora.java
@@ -3,11 +3,23 @@ package org.yourcompany.calculadora;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Hoshikuu
+ */
 public class Calculadora {
+    /**
+     * Mètode principal que inicia el menú de la calculadora
+     * 
+     * @param args Arguments de línia de comandes
+     */
     public static void main(String[] args) {
         mostrarMenuConsola();
     }
 
+    /**
+     * Mostra un menú per consola per seleccionar diferents operacions matemàtiques
+     */
     public static void mostrarMenuConsola() {
         try (Scanner teclat = new Scanner(System.in)) {
             int opcio;
@@ -30,7 +42,7 @@ public class Calculadora {
                     case 1 -> {
                         System.out.print("Entra el numero: ");
                         num = teclat.nextInt();
-                        System.out.println("Suma dels primers" + num + "números: " + sumaPrimersNumeros(num));
+                        System.out.println("Suma dels primers " + num + " números: " + sumaPrimersNumeros(num));
                     }
                     case 2 -> {
                         System.out.print("Entra el numero: ");
@@ -45,7 +57,7 @@ public class Calculadora {
                     case 4 -> {
                         System.out.print("Entra la base: ");
                         num = teclat.nextInt();
-                        System.out.print("Entra el exponent");
+                        System.out.print("Entra el exponent: ");
                         int exponent = teclat.nextInt();
                         System.out.println(num + " elevat a la potència " + exponent + ": " + calcularPotencia(num, exponent));
                     }
@@ -66,7 +78,7 @@ public class Calculadora {
                         boolean b_cap = Character.toUpperCase(teclat.next().charAt(0)) == 'S';
                         System.out.print("Te carnet jove? (S/N): ");
                         boolean b_carnet = Character.toUpperCase(teclat.next().charAt(0)) == 'S';
-                        System.out.println("El preu del cinema es: " + preuCinema(num, b_cap, b_carnet));
+                        System.out.println("El preu del cinema es: " + preuCinema(num, b_cap, b_carnet) + "€");
                     }
                     case 8 -> {
                     }
@@ -76,6 +88,12 @@ public class Calculadora {
         }
     }
 
+    /**
+     * Suma els primers números des de l'1 fins al número indicat
+     * 
+     * @param num Número fins al qual sumar
+     * @return Suma dels primers números
+     */
     public static int sumaPrimersNumeros(int num) {
         int suma = 0;
         
@@ -86,6 +104,12 @@ public class Calculadora {
         return suma;
     }
 
+    /**
+     * Calcula el factorial d'un número enter positiu
+     * 
+     * @param num Número enter positiu
+     * @return Factorial del número
+     */
     public static int calcularFactorial(int num) {
         int factorial = 1;
 
@@ -96,6 +120,12 @@ public class Calculadora {
         return factorial;
     }
 
+    /**
+     * Suma els quadrats dels primers números des de l'1 fins al número indicat
+     * 
+     * @param num Número fins al qual sumar els quadrats
+     * @return Suma dels quadrats dels primers números
+     */
     public static int sumaQuadrats(int num) {
         int suma = 0;
 
@@ -106,6 +136,13 @@ public class Calculadora {
         return suma;
     }
 
+    /**
+     * Calcula la potència d'una base elevada a un exponent
+     * 
+     * @param base Base del exponent
+     * @param exponent Exponent
+     * @return Resultat de la potència
+     */
     public static int calcularPotencia(int base, int exponent) {
         int resultat = 1;
 
@@ -116,6 +153,12 @@ public class Calculadora {
         return resultat;
     }
 
+    /**
+     * Calcula la quantitat de dígits que té un nombre enter
+     * 
+     * @param nombre Nombre enter
+     * @return Nombre de dígits que té el nombre
+     */
     public static int nombreDigits(int nombre) {
         int comptador = 0;
         
@@ -127,10 +170,15 @@ public class Calculadora {
             nombre /= 10;
             comptador++;
         }
-
         return comptador;
     }
 
+    /**
+     * Calcula el nombre de cares obtingudes en un nombre de tirades d'una moneda
+     * 
+     * @param rep Nombre de tirades
+     * @return Nombre de cares obtingudes
+     */
     public static int numeroCares(int rep) {
         Random random = new Random();
         
@@ -143,10 +191,17 @@ public class Calculadora {
                 cara++;
             }
         }
-
         return cara;
     }
 
+    /**
+     * Calcula el preu del cinema segons diferents condicions
+     * 
+     * @param preuBase Preu base de l'entrada
+     * @param esCapDeSetmana Si és cap de setmana
+     * @param teCarnetJove Si té carnet jove
+     * @return Preu final de l'entrada
+     */
     public static double preuCinema(double preuBase, boolean esCapDeSetmana, boolean teCarnetJove) {
         double preu = preuBase;
         
